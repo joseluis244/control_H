e676e44c545841cb612940b535d8384584a792ed
joseluis244 <32981592+joseluis244@users.noreply.github.com>
2018-05-07 17:51:42 -0400

    control init

--- rutas.js
+++ rutas.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
     app.get("/",function(req,res){
-        res.send("siadasudha");
+        
     });
     app.get("/main",function(req,res){
         res.render("main.ejs");
--- server.js
+++ server.js
@@ -7,19 +7,18 @@ const https = require("https");
 const forcessl = require("express-force-ssl");
 const ssl = require("./ssl");
 const rutas = require("./rutas");
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 //sets
 var puerto = JSON.parse(fs.readFileSync("./configuraciones/conf.json"));
 app.set("view engine", "ejs")
 //use
 app.use(forcessl);
 app.use(express.static("html"));
-
+app.use(cookieParser());
+app.use(session({secret: 'anagramas'}));
 //rutas
 rutas(app);
-app.use((err,req,res,next)=>{
-    console.log(err.stack);
-    res.status(500).sendfile("./html/500.html")
-});
 app.use((req,res,next)=>{res.status(404).sendfile("./html/404.html")});
 //inicio servidor
 https.createServer(ssl.ssl(),app).listen(puerto.https_puerto);
